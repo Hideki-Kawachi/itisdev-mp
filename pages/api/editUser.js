08ba915160bdab346d1023d7cfc4b9590e735725
RonHidekiK <hideki.kawachi17@gmail.com>
2022-11-22 15:20:14 +0800

    Incomplete user editing api

---
+++ pages/api/editUser.js
@@ -0,0 +1,18 @@
+import dbConnect from "../../lib/dbConnect";
+import User from "../../models/UserSchema";
+
+export default async (req, res) => {
+	await dbConnect();
+
+	const userInfo = req.body;
+
+	let invalidUserID = await User.findOneAndUpdate({ userID: userInfo.userID });
+
+	if (invalidUserID != null) {
+		console.log("INVALID");
+		res.json(userInfo.userID);
+	} else {
+		await User.create(userInfo);
+		res.json("created");
+	}
+};
