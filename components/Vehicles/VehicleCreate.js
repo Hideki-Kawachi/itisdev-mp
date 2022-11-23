f39a9053065afafd58e06d56542175a3651e00a9
MartinJ-13 <80908021+MartinJ-13@users.noreply.github.com>
2022-11-23 16:10:29 +0800

    add vehicle category popup :D

---
+++ components/Vehicles/VehicleCreate.js
@@ -0,0 +1,246 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import ToggleSwitch from "../../components/ToggleSwitch";
+import BasicButton from "../../components/BasicButton";
+import vCatTable from "../../components/Vehicles/vCategoryList";
+
+
+
+function VehicleCreate({trigger, setTrigger}) {
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  function log() {
+  console.log("trigger status is " + trigger);
+}
+  return (
+    <>
+        {/* First Field Group */}
+
+        <div className="form-container">
+          <div className="form-item">
+            <label className="form-labels">
+              Plate Number: <label className="required"> * </label>{" "}
+            </label>{" "}
+            <br />
+            <input
+              type="text"
+              className="form-fields"
+              placeholder="Enter Plate Number"
+            />
+          </div>
+          <div className="form-item">
+            <label className="form-labels">
+              Vehicle Type: <label className="required"> * </label>{" "}
+            </label>{" "}
+            <button
+              className="vehicle-icon-button vehicle-add-option-button "
+              onClick={() => {
+                 log();
+                setTrigger(!trigger);
+                log();
+              }}
+            >
+              {" "}
+              ✎{" "}
+            </button>
+            
+            <br />
+            <select
+              type="text"
+              className="form-fields"
+              placeholder="Select Vehicle Type"
+            />
+          </div>
+          <div className="form-item form-toggle">
+            {" "}
+            Status:{" "}
+            <button
+              className="item-icon-button item-info-option-button "
+              onClick={() => setTrigger(!trigger)}
+            >
+              {" "}
+              i{" "}
+            </button>
+            <br />
+            <ToggleSwitch
+              disabled={isDisabled}
+              setDisabled={setIsDisabled}
+            ></ToggleSwitch>
+          </div>
+        </div>
+
+        {/* Second Field Group */}
+        <br />
+        <div className="form-container">
+          <div className="form-item">
+            <label className="form-labels">
+              Brand: <label className="required"> * </label>{" "}
+            </label>{" "}
+            <button
+              className="vehicle-icon-button vehicle-add-option-button "
+              onClick={() => setCategoryPopupOn(true)}
+            >
+              {" "}
+              ✎{" "}
+            </button>
+            <br />
+            <select className="form-fields" />
+          </div>
+
+          <div className="form-item">
+            <label className="form-labels">
+              Manufacturing Year: <label className="required"> * </label>{" "}
+            </label>{" "}
+            <br />
+            <input
+              type="text"
+              className="form-fields"
+              placeholder="Enter Manufacturing Year"
+            />
+          </div>
+
+          <div className="form-item">
+            <label className="form-labels">
+              Transmission Type: <label className="required"> * </label>{" "}
+            </label>{" "}
+            <button
+              className="vehicle-icon-button vehicle-add-option-button "
+              onClick={() => setCategoryPopupOn(true)}
+            >
+              {" "}
+              ✎{" "}
+            </button>
+            <br />
+            <select className="form-fields" />
+          </div>
+        </div>
+
+        <br />
+        <hr />
+        {/* Third Field Row */}
+
+        <div className="form-container">
+          <div className="form-item">
+            <label className="form-labels">
+              Engine Number: <label className="required"> * </label>{" "}
+            </label>{" "}
+            <br />
+            <input
+              type="text"
+              className="form-fields"
+              placeholder="Enter Engine Number"
+            />
+          </div>
+
+          <div className="form-item">
+            <label className="form-labels">
+              Engine Type: <label className="required"> * </label>{" "}
+            </label>{" "}
+            <button
+              className="vehicle-icon-button vehicle-add-option-button "
+              onClick={() => setCategoryPopupOn(true)}
+            >
+              {" "}
+              ✎{" "}
+            </button>
+            <br />
+            <select className="form-fields" />
+          </div>
+
+          <div className="form-item">
+            <label className="form-labels">
+              Chassis: <label className="required"> * </label>{" "}
+            </label>{" "}
+            <br />
+            <input
+              type="text"
+              className="form-fields"
+              placeholder="Select Chassis"
+            />
+          </div>
+        </div>
+
+        <br />
+
+        <div className="form-container">
+          <div className="form-item">
+            <label className="form-labels">
+              Insurance Amount: <label className="required"> * </label>{" "}
+            </label>{" "}
+            <br />
+            <input
+              type="text"
+              className="form-fields"
+              placeholder="Enter Insurance Amount"
+            />
+          </div>
+
+          <div className="form-item">
+            <label className="form-labels">
+              Insurance Expiry Date: <label className="required"> * </label>{" "}
+            </label>{" "}
+            <br />
+            <input
+              type="date"
+              className="form-fields"
+              placeholder="Enter Insurance Expiry Date"
+            />
+          </div>
+        </div>
+        <br />
+        <hr />
+        <div className="form-container">
+          <div className="form-item">
+            <label className="form-labels">
+              GPS Provider Name: <label className="required"> * </label>{" "}
+            </label>{" "}
+            <button
+              className="vehicle-icon-button vehicle-add-option-button "
+              onClick={() => setCategoryPopupOn(true)}
+            >
+              {" "}
+              ✎{" "}
+            </button>
+            <br />
+            <select className="form-fields" />
+          </div>
+
+          <div className="form-item">
+            <label className="form-labels">
+              Fuel Level Sensor Name: <label className="required"> * </label>{" "}
+            </label>{" "}
+            <button
+              className="vehicle-icon-button vehicle-add-option-button "
+              onClick={() => setCategoryPopupOn(true)}
+            >
+              {" "}
+              ✎{" "}
+            </button>
+            <br />
+            <select
+              className="form-fields"
+              placeholder="Select Fuel Level Sensor Name"
+            />
+          </div>
+
+          <br />
+        </div>
+        <div className="form-container">
+          <span className="form-item-buttons">
+            <BasicButton
+              label={"Cancel"}
+              color={"gray"}
+              type={"reset"}
+            ></BasicButton>
+            <BasicButton
+              label={"Save"}
+              color={"green"}
+              type={"button"}
+            ></BasicButton>
+          </span>
+        </div>
+    </>
+  );
+}
+
+export default VehicleCreate;
--- pages/vehicles/addvehicle.js
+++ pages/vehicles/addvehicle.js
@@ -1,16 +1,18 @@
 import React, {useState} from "react";
-import Link from "next/link";
+import { Router, useRouter } from "next/router";
+
 import Header from "../../components/Header";
 import NavBar from "../../components/NavBar";
 import ToggleSwitch from "../../components/ToggleSwitch";
 import BasicButton from "../../components/BasicButton";
 import vCatTable from "../../components/Vehicles/vCategoryList";
+import VehicleCreate from "../../components/Vehicles/VehicleCreate";
+import AddVehicleCategory from "../../components/Vehicles/vCategoryCreate";
 
-function AddVehicle() {
-
-  const [isDisabled, setIsDisabled] = useState(false);
+function AddVehicle({ trigger, setTrigger }) {
+  const router = useRouter();
   const [categoryPopupOn, setCategoryPopupOn] = useState(false);
-  
+
   return (
     <>
       <Header
@@ -21,229 +23,27 @@ function AddVehicle() {
       <NavBar></NavBar>
       <br />
       <div id="main-container">
-        {/* First Field Group */}
-        <div className="form-container">
-          <div className="form-item">
-            <label className="form-labels">
-              Plate Number: <label className="required"> * </label>{" "}
-            </label>{" "}
-            <br />
-            <input
-              type="text"
-              className="form-fields"
-              placeholder="Enter Plate Number"
+        {categoryPopupOn ? (
+          <>
+            <div className="item-modal-bg">
+              <AddVehicleCategory
+                trigger={categoryPopupOn}
+                setTrigger={setCategoryPopupOn}
+              />
+            </div>
+            <VehicleCreate
+              trigger={categoryPopupOn}
+              setTrigger={setCategoryPopupOn}
+            />
+          </>
+        ) : (
+          <div id="main-container-bg">
+            <VehicleCreate
+              trigger={categoryPopupOn}
+              setTrigger={setCategoryPopupOn}
             />
           </div>
-          <div class="form-item">
-            <label className="form-labels">
-              Vehicle Type: <label className="required"> * </label>{" "}
-            </label>{" "}
-            <button
-              className="vehicle-icon-button vehicle-add-option-button "
-              onClick={() => setCategoryPopupOn(true)}
-            >
-              {" "}
-              ✎{" "}
-            </button>
-            <br />
-            <select
-              type="text"
-              className="form-fields"
-              placeholder="Select Vehicle Type"
-            />
-          </div>
-          <div class="form-item form-toggle">
-            {" "}
-            Status:{" "}
-            <button
-              className="item-icon-button item-info-option-button "
-              onClick={() => setTrigger(!trigger)}
-            >
-              {" "}
-              i{" "}
-            </button>
-            <br />
-            <ToggleSwitch
-              disabled={isDisabled}
-              setDisabled={setIsDisabled}
-            ></ToggleSwitch>
-          </div>
-        </div>
-
-        {/* Second Field Group */}
-        <br />
-        <div className="form-container">
-          <div className="form-item">
-            <label className="form-labels">
-              Brand: <label className="required"> * </label>{" "}
-            </label>{" "}
-            <button
-              className="vehicle-icon-button vehicle-add-option-button "
-              onClick={() => setTrigger(!trigger)}
-            >
-              {" "}
-              ✎{" "}
-            </button>
-            <br />
-            <select className="form-fields" />
-          </div>
-
-          <div className="form-item">
-            <label className="form-labels">
-              Manufacturing Year: <label className="required"> * </label>{" "}
-            </label>{" "}
-            <br />
-            <input
-              type="text"
-              className="form-fields"
-              placeholder="Enter Manufacturing Year"
-            />
-          </div>
-
-          <div className="form-item">
-            <label className="form-labels">
-              Transmission Type: <label className="required"> * </label>{" "}
-            </label>{" "}
-            <button
-              className="vehicle-icon-button vehicle-add-option-button "
-              onClick={() => setTrigger(!trigger)}
-            >
-              {" "}
-              ✎{" "}
-            </button>
-            <br />
-            <select className="form-fields" />
-          </div>
-        </div>
-
-        <br />
-        <hr />
-        {/* Third Field Row */}
-
-        <div className="form-container">
-          <div className="form-item">
-            <label className="form-labels">
-              Engine Number: <label className="required"> * </label>{" "}
-            </label>{" "}
-            <br />
-            <input
-              type="text"
-              className="form-fields"
-              placeholder="Enter Engine Number"
-            />
-          </div>
-
-          <div className="form-item">
-            <label className="form-labels">
-              Engine Type: <label className="required"> * </label>{" "}
-            </label>{" "}
-            <button
-              className="vehicle-icon-button vehicle-add-option-button "
-              onClick={() => setTrigger(!trigger)}
-            >
-              {" "}
-              ✎{" "}
-            </button>
-            <br />
-            <select className="form-fields" />
-          </div>
-
-          <div className="form-item">
-            <label className="form-labels">
-              Chassis: <label className="required"> * </label>{" "}
-            </label>{" "}
-            <br />
-            <input
-              type="text"
-              className="form-fields"
-              placeholder="Select Chassis"
-            />
-          </div>
-        </div>
-
-        <br />
-
-        <div className="form-container">
-          <div className="form-item">
-            <label className="form-labels">
-              Insurance Amount: <label className="required"> * </label>{" "}
-            </label>{" "}
-            <br />
-            <input
-              type="text"
-              className="form-fields"
-              placeholder="Enter Insurance Amount"
-            />
-          </div>
-
-          <div className="form-item">
-            <label className="form-labels">
-              Insurance Expiry Date: <label className="required"> * </label>{" "}
-            </label>{" "}
-            <br />
-            <input
-              type="date"
-              className="form-fields"
-              placeholder="Enter Insurance Expiry Date"
-            />
-          </div>
-        </div>
-        <br />
-        <hr />
-        <div className="form-container">
-          <div className="form-item">
-            <label className="form-labels">
-              GPS Provider Name: <label className="required"> * </label>{" "}
-            </label>{" "}
-            <button
-              className="vehicle-icon-button vehicle-add-option-button "
-              onClick={() => setTrigger(!trigger)}
-            >
-              {" "}
-              ✎{" "}
-            </button>
-            <br />
-            <select className="form-fields" />
-          </div>
-
-          <div className="form-item">
-            <label className="form-labels">
-              Fuel Level Sensor Name: <label className="required"> * </label>{" "}
-            </label>{" "}
-            <button
-              className="vehicle-icon-button vehicle-add-option-button "
-              onClick={() => setTrigger(!trigger)}
-            >
-              {" "}
-              ✎{" "}
-            </button>
-            <br />
-            <select
-              className="form-fields"
-              placeholder="Select Fuel Level Sensor Name"
-            />
-          </div>
-
-          <br />
-        </div>
-        <div className="form-container">
-          <span className="form-item-buttons">
-            <BasicButton
-              label={"Cancel"}
-              color={"gray"}
-              type={"reset"}
-            ></BasicButton>
-            <BasicButton
-              label={"Save"}
-              color={"green"}
-              type={"button"}
-            ></BasicButton>
-          </span>
-        </div>
-        <vCatTable
-          trigger={categoryPopupOn}
-          setTrigger={setCategoryPopupOn}
-        >test</vCatTable>
+        )}
       </div>
     </>
   );
