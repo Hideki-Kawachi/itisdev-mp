b367528667be087767241cf4e805a14903d26dac
MartinJ-13 <80908021+MartinJ-13@users.noreply.github.com>
2022-12-13 23:58:52 +0800

    Add and Pull columns

---
+++ components/Reports/AddColumns.js
@@ -0,0 +1,50 @@
+import { GlobalFilter } from "../GlobalFilter";
+
+export const COLUMNS = [
+  {
+    Header: "",
+    id: "index",
+    accessor: (/** @type {any} */ _row, /** @type {number} */ i) => i + 1,
+  },
+  {
+    Header: "Date",
+    accessor: "creationDate",
+    disableGlobalFilter: true,
+    filter: (rows, id, filterValue) => {
+      return rows.filter(
+        (row) =>
+          filterValue.length <= 0 ||
+          !filterValue ||
+          filterValue.includes(row.values[id])
+      );
+    },
+  },
+  {
+    Header: "Invoice #",
+    accessor: "invoiceNumber",
+    Filter: GlobalFilter,
+    disableSortBy: true,
+  },
+  {
+    Header: "Item",
+    accessor: "itemID",
+    disableGlobalFilter: true,
+    disableSortBy: true,
+  },
+  {
+    Header: "Model",
+    accessor: "itemModel",
+    disableGlobalFilter: true,
+  },
+  {
+    Header: "Quantity",
+    accessor: "quantity",
+    disableGlobalFilter: true,
+  },
+  {
+    Header: "Unit",
+    accessor: "Unit",
+    disableGlobalFilter: true,
+    disableSortBy: true,
+  },
+];
