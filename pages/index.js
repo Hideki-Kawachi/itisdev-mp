3dce79658e2c8d9e38879cda78688127b69a8ddb
RonHidekiK <hideki.kawachi17@gmail.com>
2022-12-13 20:58:14 +0800

    Dashboard Layout

--- pages/index.js
+++ pages/index.js
@@ -28,31 +28,47 @@ export const getServerSideProps = withIronSessionSsr(
 );
 
 const Index = ({ currentUser }) => {
+	const totalUsers = 3;
+	const totalVehicles = 35;
+	const inFlow = 45;
+	const outFlow = 72;
+
 	return (
 		<>
 			<Header page={"DASHBOARD"} subPage={"HOME"} user={currentUser}></Header>
 			<NavBar user={currentUser}></NavBar>
-			<div id="main-container">
-				<p>dashboard</p>
+			<div id="main-container" className="dashboard-main-container">
+				<div className="dashboard-left-container">
+					<div className="dashboard-top-container">
+						<div>
+							<h1>Total Users</h1>
+							<span>{totalUsers}</span>
+						</div>
+						<div>
+							<h1>Total Vehicles</h1>
+							<span>{totalVehicles}</span>
+						</div>
+						<div>
+							<h1>In-Flow: Past 30 Days</h1>
+							<span>{inFlow}</span>
+						</div>
+					</div>
+					<div className="dashboard-table-container">
+						<h1>Recent Inventory Records</h1>
+					</div>
+				</div>
+				<div className="dashboard-right-container">
+					<div>
+						<h1>Out-Flow: Past 30 Days</h1>
+						<span>{outFlow}</span>
+					</div>
+					<div className="dashboard-stock-container">
+						<h1>Low Stock</h1>
+					</div>
+				</div>
 			</div>
 		</>
 	);
 };
-
-/* Retrieves pet(s) data from mongodb database */
-// export async function getServerSideProps() {
-
-// 	await dbConnect();
-
-// 	/* find all the data in our database */
-// 	const result = await Pet.find({});
-// 	const pets = result.map((doc) => {
-// 		const pet = doc.toObject();
-// 		pet._id = pet._id.toString();
-// 		return pet;
-// 	});
-
-// 	return { props: { pets: pets } };
-// }
 
 export default Index;
