43a0e718f81609ab1172f5b62bb0ceb01cb3726b
RonHidekiK <hideki.kawachi17@gmail.com>
2022-12-13 22:51:43 +0800

    Update BasicTable.js

--- components/Reports/BasicTable.js
+++ components/Reports/BasicTable.js
@@ -2,137 +2,128 @@ import React, { useMemo, useState } from "react";
 import dayjs from "dayjs";
 
 import {
-  useTable,
-  useSortBy,
-  useGlobalFilter,
-  usePagination,
-  useFilters
+	useTable,
+	useSortBy,
+	useGlobalFilter,
+	usePagination,
+	useFilters,
 } from "react-table";
 
 import GlobalFilter from "../GlobalFilter";
 
 export const BasicTable = (props) => {
+	const columns = useMemo(() => props.COLUMNS, []);
+	const data = useMemo(() => props.ADDINV, []);
 
-  const columns = useMemo(() => props.COLUMNS, []);
-  const data = useMemo(() => props.ADDINV, []);
- 
- const [fromDate, setFromDate] = useState();
- const [toDate, setToDate] = useState();
- const {
-   getTableProps,
-   getTableBodyProps,
-   headerGroups,
-   page,
-   nextPage,
-   canNextPage,
-   previousPage,
-   canPreviousPage,
-   gotoPage,
-   pageOptions,
-   pageCount,
-   prepareRow,
-   setPageSize,
-   setFilter,
-   filters,
-   state,
-   setGlobalFilter,
- } = useTable(
-   {
-     columns,
-     data,
-   },
+	const [fromDate, setFromDate] = useState();
+	const [toDate, setToDate] = useState();
+	const {
+		getTableProps,
+		getTableBodyProps,
+		headerGroups,
+		page,
+		nextPage,
+		canNextPage,
+		previousPage,
+		canPreviousPage,
+		gotoPage,
+		pageOptions,
+		pageCount,
+		prepareRow,
+		setPageSize,
+		setFilter,
+		filters,
+		state,
+		setGlobalFilter,
+	} = useTable(
+		{
+			columns,
+			data,
+		},
 
-   useGlobalFilter,
-   useFilters,
-   useSortBy,
-   usePagination
- );
+		useGlobalFilter,
+		useFilters,
+		useSortBy,
+		usePagination
+	);
 
- const { globalFilter } = state;
- const { pageIndex } = state;
+	const { globalFilter } = state;
+	const { pageIndex } = state;
 
- const handleFilter = () => {
-    const dateArray = [];
-    var d = dayjs(fromDate);
+	const handleFilter = () => {
+		const dateArray = [];
+		var d = dayjs(fromDate);
 
-      while (d.isBefore(toDate, "day") || d.isSame(toDate, "day")) {
-        console.log("Date: " + d);
-        dateArray.push(d.format("MM/DD/YYYY"));
-        d = d.add(1, "day");
-      }
-      console.log(dateArray);
-      filterTable(dateArray);
-      console.log("From Date: " + fromDate + " To Date: " + toDate);
- };
+		while (d.isBefore(toDate, "day") || d.isSame(toDate, "day")) {
+			dateArray.push(d.format("MM/DD/YYYY"));
+			d = d.add(1, "day");
+		}
+		filterTable(dateArray);
+	};
 
-const filterTable = (dates) => {
-     if (useTable.current) {
-       useTable.current.setFilter("date", dates);
-    }
- };
- 
-  return (
-    <div>
-      <br />
-      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
-      <span className="calendar-range-container">
-        <input
-          type="date"
-          className="form-fields form-fields-calendar-range"
-          onChange={(e) =>
-            setFromDate(dayjs(e.target.value, "MM/DD/YYYY"))
-          }
-        />{" "}
-        to{" "}
-        <input
-          type="date"
-          className="form-fields form-fields-calendar-range"
-          onChange={(e) =>
-            setToDate(dayjs(e.target.value, "MM/DD/YYYY"))
-          }
-        />
-        <button onClick={handleFilter}>Check Date</button>
-      </span>
+	const filterTable = (dates) => {
+		console.log("Dates are:", dates);
+		setFilter("date", dates);
+	};
 
-      <table id="btable" {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr
-              id="btable"
-              className="btable"
-              {...headerGroup.getHeaderGroupProps()}
-            >
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                  {column.render("Header")}
-                  <span>
-                    {column.isSorted ? (column.isSortedDesc ? "▼" : "▲") : " "}
-                  </span>
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {page.map((row) => {
-            prepareRow(row);
-            return (
-              <tr id="btable" {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+	return (
+		<div>
+			<br />
+			<GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+			<span className="calendar-range-container">
+				<input
+					type="date"
+					className="form-fields form-fields-calendar-range"
+					onChange={(e) => setFromDate(dayjs(e.target.value, "MM/DD/YYYY"))}
+				/>{" "}
+				to{" "}
+				<input
+					type="date"
+					className="form-fields form-fields-calendar-range"
+					onChange={(e) => setToDate(dayjs(e.target.value, "MM/DD/YYYY"))}
+				/>
+				<button onClick={handleFilter}>Check Date</button>
+			</span>
 
-      <br />
+			<table id="btable" {...getTableProps()}>
+				<thead>
+					{headerGroups.map((headerGroup) => (
+						<tr
+							id="btable"
+							className="btable"
+							{...headerGroup.getHeaderGroupProps()}
+						>
+							{headerGroup.headers.map((column) => (
+								<th {...column.getHeaderProps(column.getSortByToggleProps())}>
+									{column.render("Header")}
+									<span>
+										{column.isSorted ? (column.isSortedDesc ? "▼" : "▲") : " "}
+									</span>
+								</th>
+							))}
+						</tr>
+					))}
+				</thead>
+				<tbody {...getTableBodyProps()}>
+					{page.map((row) => {
+						prepareRow(row);
+						return (
+							<tr id="btable" {...row.getRowProps()}>
+								{row.cells.map((cell) => {
+									return (
+										<td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+									);
+								})}
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
 
-      <div className="page-buttons">
-        {/* <input
+			<br />
+
+			<div className="page-buttons">
+				{/* <input
           type="number"
           defaultValue={pageIndex + 1}
           onChange={(e) => {
@@ -140,45 +131,45 @@ const filterTable = (dates) => {
             gotoPage(pageNumber);
           }}
         /> */}
-        <span>
-          Page{" "}
-          <strong>
-            {pageIndex + 1} of {pageOptions.length}
-          </strong>
-        </span>
+				<span>
+					Page{" "}
+					<strong>
+						{pageIndex + 1} of {pageOptions.length}
+					</strong>
+				</span>
 
-        <span className="vehicle-nav-buttons-div">
-          <button
-            className="navigate-page"
-            onClick={() => gotoPage(0)}
-            disabled={!canPreviousPage}
-          >
-            {"<<"}
-          </button>
+				<span className="vehicle-nav-buttons-div">
+					<button
+						className="navigate-page"
+						onClick={() => gotoPage(0)}
+						disabled={!canPreviousPage}
+					>
+						{"<<"}
+					</button>
 
-          <button
-            className="navigate-page"
-            onClick={() => previousPage()}
-            disabled={!canPreviousPage}
-          >
-            {"<"}
-          </button>
-          <button
-            className="navigate-page"
-            onClick={() => nextPage()}
-            disabled={!canNextPage}
-          >
-            {">"}
-          </button>
-          <button
-            className="navigate-page"
-            onClick={() => gotoPage(pageCount - 1)}
-            disabled={!canNextPage}
-          >
-            {">>"}
-          </button>
-        </span>
-      </div>
-    </div>
-  );
+					<button
+						className="navigate-page"
+						onClick={() => previousPage()}
+						disabled={!canPreviousPage}
+					>
+						{"<"}
+					</button>
+					<button
+						className="navigate-page"
+						onClick={() => nextPage()}
+						disabled={!canNextPage}
+					>
+						{">"}
+					</button>
+					<button
+						className="navigate-page"
+						onClick={() => gotoPage(pageCount - 1)}
+						disabled={!canNextPage}
+					>
+						{">>"}
+					</button>
+				</span>
+			</div>
+		</div>
+	);
 };
