d8f1c02ac1f18034128e7188d1d28fb9fdae8efc
spensur <daniel_spencer_tan@dlsu.edu.ph>
2022-12-14 18:22:48 +0800

    Added Measure edit and delete
    -not yet bug free 🪲

--- pages/api/measures/deleteMeasure.js
+++ pages/api/measures/deleteMeasure.js
@@ -1,4 +1,4 @@
-import dbConnect from "../../lib/dbConnect";
+import dbConnect from "../../../lib/dbConnect";
 import Measure from "../../../models/MeasureSchema";
 
 export default async (req, res) => {
