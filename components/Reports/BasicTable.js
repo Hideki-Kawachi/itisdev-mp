19efb969c40989b543bc7083581463867e0a7024
MartinJ-13 <80908021+MartinJ-13@users.noreply.github.com>
2022-12-15 13:26:02 +0800

    fixed report date range styling

--- components/Reports/BasicTable.js
+++ components/Reports/BasicTable.js
@@ -80,63 +80,64 @@ export const BasicTable = (props) => {
   }, [fromDate,toDate]);
 
 	return (
-		<div>
-			<br />
-			<GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
-			<span className="calendar-range-container">
-				<input
-					type="date"
-					className="form-fields form-fields-calendar-range"
-					onChange={(e) => setFromDate(dayjs(e.target.value, "MM/DD/YYYY"))}
-				/>{" "}
-				to{" "}
-				<input
-					type="date"
-					className="form-fields form-fields-calendar-range"
-					onChange={(e) => setToDate(dayjs(e.target.value, "MM/DD/YYYY"))}
-				/>
-				{/* <button onClick={handleFilter}>Check Date</button> */}
-			</span>
+    <div>
+      <br />
+      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+      <div className="calendar-range-container">
+        <input
+          type="date"
+          className="form-fields form-fields-calendar-range"
+          onChange={(e) => setFromDate(dayjs(e.target.value, "MM/DD/YYYY"))}
+        />{" "}
+        to{" "}
+        <input
+          type="date"
+          className="form-fields form-fields-calendar-range"
+          onChange={(e) => setToDate(dayjs(e.target.value, "MM/DD/YYYY"))}
+        />
+        {/* <button onClick={handleFilter}>Check Date</button> */}
+      </div>
+      <br />
+      <br />
+      <table id="btable" {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr
+              id="btable"
+              className="btable"
+              {...headerGroup.getHeaderGroupProps()}
+            >
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  <span>
+                    {column.isSorted ? (column.isSortedDesc ? "▼" : "▲") : " "}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr id="btable" {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
 
-			<table id="btable" {...getTableProps()}>
-				<thead>
-					{headerGroups.map((headerGroup) => (
-						<tr
-							id="btable"
-							className="btable"
-							{...headerGroup.getHeaderGroupProps()}
-						>
-							{headerGroup.headers.map((column) => (
-								<th {...column.getHeaderProps(column.getSortByToggleProps())}>
-									{column.render("Header")}
-									<span>
-										{column.isSorted ? (column.isSortedDesc ? "▼" : "▲") : " "}
-									</span>
-								</th>
-							))}
-						</tr>
-					))}
-				</thead>
-				<tbody {...getTableBodyProps()}>
-					{page.map((row) => {
-						prepareRow(row);
-						return (
-							<tr id="btable" {...row.getRowProps()}>
-								{row.cells.map((cell) => {
-									return (
-										<td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-									);
-								})}
-							</tr>
-						);
-					})}
-				</tbody>
-			</table>
+      <br />
 
-			<br />
-
-			<div className="page-buttons">
-				{/* <input
+      <div className="page-buttons">
+        {/* <input
           type="number"
           defaultValue={pageIndex + 1}
           onChange={(e) => {
@@ -144,45 +145,45 @@ export const BasicTable = (props) => {
             gotoPage(pageNumber);
           }}
         /> */}
-				<span>
-					Page{" "}
-					<strong>
-						{pageIndex + 1} of {pageOptions.length}
-					</strong>
-				</span>
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>
+        </span>
 
-				<span className="vehicle-nav-buttons-div">
-					<button
-						className="navigate-page"
-						onClick={() => gotoPage(0)}
-						disabled={!canPreviousPage}
-					>
-						{"<<"}
-					</button>
+        <span className="vehicle-nav-buttons-div">
+          <button
+            className="navigate-page"
+            onClick={() => gotoPage(0)}
+            disabled={!canPreviousPage}
+          >
+            {"<<"}
+          </button>
 
-					<button
-						className="navigate-page"
-						onClick={() => previousPage()}
-						disabled={!canPreviousPage}
-					>
-						{"<"}
-					</button>
-					<button
-						className="navigate-page"
-						onClick={() => nextPage()}
-						disabled={!canNextPage}
-					>
-						{">"}
-					</button>
-					<button
-						className="navigate-page"
-						onClick={() => gotoPage(pageCount - 1)}
-						disabled={!canNextPage}
-					>
-						{">>"}
-					</button>
-				</span>
-			</div>
-		</div>
-	);
+          <button
+            className="navigate-page"
+            onClick={() => previousPage()}
+            disabled={!canPreviousPage}
+          >
+            {"<"}
+          </button>
+          <button
+            className="navigate-page"
+            onClick={() => nextPage()}
+            disabled={!canNextPage}
+          >
+            {">"}
+          </button>
+          <button
+            className="navigate-page"
+            onClick={() => gotoPage(pageCount - 1)}
+            disabled={!canNextPage}
+          >
+            {">>"}
+          </button>
+        </span>
+      </div>
+    </div>
+  );
 };
