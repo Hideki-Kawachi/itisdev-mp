06a1f7c986c4a5b7656a08a7dee03e43a1a00fd2
RonHidekiK <hideki.kawachi17@gmail.com>
2022-12-15 15:15:46 +0800

    updated index

--- components/DashboardCard.js
+++ components/DashboardCard.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-function DashboardCard({ itemName, itemModel, quantity }) {
+function DashboardCard({ itemName, itemModel, reorder, quantity }) {
 	return (
 		<div className="dashboard-card-main-container">
 			<div className="dashboard-card-left-container">
 				<h1>{itemName}</h1>
 				<span>{itemModel}</span>
 			</div>
+			<h1 className="dashboard-card-reorder">{reorder}</h1>
 			<h1 className="dashboard-card-count">{quantity}</h1>
 		</div>
 	);
